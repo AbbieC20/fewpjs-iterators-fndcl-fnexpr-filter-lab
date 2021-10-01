1a59ecb1df60fc8a64ca4409e0ba2705173b5882
Abbie Cornelius <abbyc@sky.com>
2021-10-01 17:58:14 +0100

    commit tree 2ac1b3c11e69f840befd757795e1d2cdc675ca44

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solution here
+function findMatching(source, sought) {
+    return source.filter( possibleMatch =>
+        possibleMatch.toLowerCase() === sought.toLowerCase()
+    )
+}
+
+function fuzzyMatch(source, sought) {
+    return source.filter( fuzzyMatch =>
+        fuzzyMatch.toLowerCase().indexOf(sought.toLowerCase()) === 0
+    )
+}
+
+function matchName(source, sought) {
+    return source.filter( record => record.name === sought)
+}
